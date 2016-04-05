6c55b1c837b8d7ab159f793fed208cecbe5aa113
rochiAmaya <rochiamaya@gmail.com>
2016-04-06 01:01:14 +0200

    Update angularApp.js

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -37,6 +37,7 @@ function($stateProvider, $urlRouterProvider) {
 	    }]
 	  }
 	})
+	/*
     .state('posts', {
 	  url: '/posts/{id}',
 	  templateUrl: '/posts.html',
@@ -47,251 +48,27 @@ function($stateProvider, $urlRouterProvider) {
 	    }]
 	  }
 	})
-
+*/
 	;
 
   $urlRouterProvider.otherwise('home');
 }])
 
-//////////////////////////////////////////////////////////////////////////////////////
-
-//Manejo de auth
-
-.factory('auth', ['$http', '$window', function($http, $window){
-   var auth = {};
-
-	auth.saveToken = function (token){
-	$window.localStorage['flapper-news-token'] = token;
-	};
-
-	auth.getToken = function (){
-	return $window.localStorage['flapper-news-token'];
-	};
-
-	auth.isLoggedIn = function(){
-		var token = auth.getToken();
-
-		if(token){
-			var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-			return payload.exp > Date.now() / 1000;
-		} else {
-			return false;
-		}
-	};
-
-	auth.currentUser = function(){
-		if(auth.isLoggedIn()){
-			var token = auth.getToken();
-			var payload = JSON.parse($window.atob(token.split('.')[1]));
-
-			return payload.username;
-		}
-	};
-
-	auth.register = function(user){
-	  return $http.post('/register', user).success(function(data){
-	    auth.saveToken(data.token);
-	  });
-	};
-
-	auth.logIn = function(user){
-	  return $http.post('/login', user).success(function(data){
-	    auth.saveToken(data.token);
-	  });
-	};
-
-	auth.logOut = function(){
-	  $window.localStorage.removeItem('flapper-news-token');
-	};
-
-  return auth;
-}])
-
-
-.controller('AuthCtrl', ['$scope','$state','auth',
-	function($scope, $state, auth){
-	  $scope.user = {};
-
-	  $scope.register = function(){
-	    auth.register($scope.user).error(function(error){
-	      $scope.error = error;
-	    }).then(function(){
-	      $state.go('home');
-	    });
-	  };
-
-	  $scope.logIn = function(){
-	    auth.logIn($scope.user).error(function(error){
-	      $scope.error = error;
-	    }).then(function(){
-	      $state.go('home');
-	    });
-	  };
-}])
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Manejo de navegacion
-
-.controller('NavCtrl', ['$scope','auth',
-	function($scope, auth){
-		$scope.isLoggedIn = auth.isLoggedIn;
-		$scope.currentUser = auth.currentUser;
-		$scope.logOut = auth.logOut;
-}]);
-
-
-
-
-
-
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Manejo de Post Object
-
-app.factory('posts', ['$http', 'auth', function($http, auth){
-
-  var o = {
-    posts: []
-  };
-
-
-   o.getAll = function () {
-      return $http.get('/posts')
-        .success(function (data) {
-          angular.copy(data, o.posts);
-      });
-  };
-
-
-	o.create = function(post) {
-	  return $http.post('/posts', post, {
-	    headers: {Authorization: 'Bearer '+auth.getToken()}
-	  }).success(function(data){
-	    o.posts.push(data);
-	  });
-	};
-
-	o.upvote = function(post) {
-	  return $http.put('/posts/' + post._id + '/upvote', null, {
-	    headers: {Authorization: 'Bearer '+auth.getToken()}
-	  }).success(function(data){
-	    post.upvotes += 1;
-	  });
-	};
-
-	o.downvote = function(post) {
-	  return $http.put('/posts/' + post._id + '/downvote', null, {
-	    headers: {Authorization: 'Bearer '+auth.getToken()}
-	  }).success(function(data){
-	    post.upvotes -= 1;
-	  });
-	};
-
-	
-	o.get = function(id) {
-	  return $http.get('/posts/' + id).then(function(res){
-	    return res.data;
-	  });
-	};
-
-	o.getComment = function(id, comment) {
-		return $http.get('/posts/' + post._id + '/comments').then(function(res){
-	    return res.data;
-	  });
-	}
-
-
-o.addComment = function(id, comment) {
-  return $http.post('/posts/' + id + '/comments', comment, {
-    headers: {Authorization: 'Bearer '+auth.getToken()}
-  });
-};
-
-o.upvoteComment = function(post, comment) {
-  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
-    headers: {Authorization: 'Bearer '+auth.getToken()}
-  }).success(function(data){
-    comment.upvotes += 1;
-  });
-};
-
-o.downvoteComment = function(post, comment) {
-  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/downvote', null, {
-    headers: {Authorization: 'Bearer '+auth.getToken()}
-  }).success(function(data){
-    comment.upvotes -= 1;
-  });
-};
-
-
-/*
-	o.addComment = function(id, comment) {
-	  return $http.post('/posts/' + id + '/comments', comment);
-	};
-
-	o.upvoteComment = function(post, comment) {
-	  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
-	    .success(function(data){
-	      comment.upvotes += 1;
-	    });
-	    
-	};
-
-	o.downvoteComment = function(post, comment) {
-	  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/downvote')
-	    .success(function(data){
-	      comment.upvotes -= 1;
-	    });
-
-	 };*/
-
-  return o;
-
-}])
-
-
-app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts','post', 'auth',
-function($scope, $stateParams, posts, post, auth){
-
-	$scope.post = post;
-
-	$scope.isLoggedIn = auth.isLoggedIn;
-
-
-	$scope.addComment = function(){
-	  if($scope.body === '') { return; }
-
-	  posts.addComment(post._id, {
-	    body: $scope.body
-	  }).success(function(comment) {
-	    $scope.post.comments.push(comment);
-	  });
-	  $scope.body = '';
-	};
-
-	$scope.incrementUpvotes = function(comment){
-		posts.upvoteComment(post, comment);
-	};
-
-
-	$scope.decrementUpvotes = function(comment){
-		posts.downvoteComment(post, comment);
-	};
-
-
-}]);
 
 
 
 app.controller('MainCtrl', ['$scope','posts','auth',
 function($scope, posts, auth){
 
-	$scope.posts = posts.posts;
-
 	$scope.isLoggedIn = auth.isLoggedIn;
+	
+	//$scope.posts = posts.posts;
 
+
+/*
 
 //Cosas para ordenar
 	$scope.predicate = 'upvotes';
@@ -321,7 +98,7 @@ function($scope, posts, auth){
 	  $scope.title = '';
 	  $scope.link = '';
 	};
-
+*/
 }]);
 
 
