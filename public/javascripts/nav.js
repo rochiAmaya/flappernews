3268861c129a237c087227eb59e78feb3278a797
rochiAmaya <rochiamaya@gmail.com>
2016-04-06 00:58:41 +0200

    Create nav.js

---
+++ public/javascripts/nav.js
@@ -0,0 +1,8 @@
+//TODO module
+
+.controller('NavCtrl', ['$scope','auth',
+	function($scope, auth){
+		$scope.isLoggedIn = auth.isLoggedIn;
+		$scope.currentUser = auth.currentUser;
+		$scope.logOut = auth.logOut;
+}]);
