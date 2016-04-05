e927df9b50e3c0c1cebba74ed680cbc6c22b2f55
rochiAmaya <rochiamaya@gmail.com>
2016-04-06 00:36:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/news');
-require('./models/Posts');
+/*require('./models/Posts');
 require('./models/Comments');
+*/
 require('./models/Users');
 require('./config/passport');
 
