d03f8137aec6e5e56132ac98752fa0d087ed4649
rochiamaya <rochiamaya@gmail.com>
2016-04-12 19:13:41 -0300

    prueba de permisos git

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 
+
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/news');
 require('./models/Posts');
