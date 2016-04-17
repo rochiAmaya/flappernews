9196b650d28f9780dfeb82b903682a4e04fb13d3
rochiamaya <rochiamaya@gmail.com>
2016-04-16 22:17:33 -0300

    Maquina de estados en mongo

--- models/Ideas.js
+++ models/Ideas.js
@@ -8,4 +8,26 @@ var IdeaSchema = new mongoose.Schema({
     /*TODO materias y tags*/
 });
 
+/*Metodos de modificacion de estados*/
+IdeaSchema.methods.estadoEliminado = function (cb) {
+    this.estado = "Eliminado";
+    this.save(cb);
+};
+
+IdeaSchema.methods.estadoRevision = function (cb) {
+    this.estado = "En Revisión";
+
+    this.save(cb);
+};
+
+IdeaSchema.methods.estadoAceptada = function (cb) {
+    this.estado = "Aceptada";
+    this.save(cb);
+};
+
+IdeaSchema.methods.estadoRechazada = function (cb) {
+    this.estado = "Rechazada";
+    this.save(cb);
+};
+
 mongoose.model('Idea', IdeaSchema);
