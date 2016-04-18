7e8b399c5b96c2e866c1a3bd2536fb0b6bbc5415
natirodriguez <nati.stefania@gmail.com>
2016-04-18 10:44:52 -0300

    Update Actividad.js

--- models/Actividad.js
+++ models/Actividad.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose'), Schema = mongoose.Schema;
 
 var ActividadSchema = new mongoose.Schema({
-    descripcion: String
+    descripcion: String, 
+    fecha: Date,
+    usuario: String
 });
 
 mongoose.model('Actividad', ActividadSchema);
