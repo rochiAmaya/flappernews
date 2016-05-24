a40acbaeead61879d15215d07a0f5973d3665bb7
rocioRedbee <rocio.amaya@redb.ee>
2016-05-24 19:52:39 -0300

    main-bower-files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require("gulp");
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var mainBowerFiles = require('gulp-main-bower-files');
 
 gulp.task('lint', function() {
     return gulp.src('*.js')
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
+});
+
+/*
+ No exponer toda la carpeta bower_components, sino sólo los archivos necesarios, utilizando el plugin "gulp-main-bower-files"
+ */
+gulp.task('main-bower-files', function() {
+    return gulp.src('./bower.json')
+        .pipe(mainBowerFiles([[filter],options][callback]))
+    .pipe(gulp.dest('./wwwroot/libs')); //TODO ver su la ruta destino es la correcta
 });
